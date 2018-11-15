d00c20bdd555af69e271229bba2ae07bb822ad8e
Dan Ward <dtward@gmail.com>
2018-11-15 15:06:15 -0500

    added Configuration basic Octo Server settings tests

--- Tests/Configuration_Private.Tests.ps1
+++ Tests/Configuration_Private.Tests.ps1
@@ -28,9 +28,9 @@ Describe 'Configuration: not initialized' {
 
   It 'Get-ODUConfig returns $null' { Get-ODUConfig | Should BeNullOrEmpty }
 
-  It 'Get-ODUConfigDecryptApiKey throws error' { { Get-ODUConfigDecryptApiKey } | Should throw }
-  
   It 'Get-ODUConfigOctopusServer throws error' { { Get-ODUConfigOctopusServer } | Should throw }
+
+  It 'Get-ODUConfigDecryptApiKey throws error' { { Get-ODUConfigDecryptApiKey } | Should throw }
 
   It 'Initialize-ODUConfig returns $null but behind the scenes Save-ODUConfig returns an initialize configuration' { 
     Initialize-ODUConfig | Should BeNullOrEmpty
@@ -42,16 +42,18 @@ Describe 'Configuration: not initialized' {
 #endregion
 
 
-
 #region Configuration only export root folder initialized
 Describe 'Configuration: only export root folder initialized' {
 
-# ensure config file DOES exist
-$ExportRootFolder = 'TestDrive:\ExportRoot'
-New-Item -Path $ExportRootFolder -ItemType directory > $null
-New-Item -Path 'TestDrive:\Configuration' -ItemType directory > $null
-$ConfigFilePath = Join-Path -Path $TestDrive "Configuration\Configuration.psd1"
-$ConfigString = @"
+  # ensure config file DOES exist
+  $ExportRootFolder = Join-Path -Path $TestDrive 'ExportRoot'
+  $ConfigFolderPath = Join-Path -Path $TestDrive 'Configuration'
+  $ConfigFilePath = Join-Path -Path $ConfigFolderPath 'Configuration.psd1'
+
+  New-Item -Path $ExportRootFolder -ItemType Directory > $null
+  New-Item -Path $ConfigFolderPath -ItemType Directory > $null
+
+  $ConfigString = @"
 @{
 ExportRootFolder = $ExportRootFolder
 OctopusServers = @()
@@ -62,56 +64,147 @@ ExternalTools = @{
 ParallelJobsCount = 1
 }
 "@
-Set-Content -Path $ConfigFilePath -Value $ConfigString
-Mock -CommandName 'Get-ODUConfigFilePath' -MockWith { $ConfigFilePath }
+  Set-Content -Path $ConfigFilePath -Value $ConfigString
+  Mock -CommandName 'Get-ODUConfigFilePath' -MockWith { $ConfigFilePath }
 
-$Config = @{
-  ExportRootFolder = $ExportRootFolder
-  OctopusServers = @()
-  ExternalTools = @{
-    DiffViewerPath = 'UNDEFINED'
-    TextEditorPath = 'UNDEFINED'
+  $Config = @{
+    ExportRootFolder  = $ExportRootFolder
+    OctopusServers    = @()
+    ExternalTools     = @{
+      DiffViewerPath = 'UNDEFINED'
+      TextEditorPath = 'UNDEFINED'
+    }
+    ParallelJobsCount = 1
   }
-  ParallelJobsCount = 1
+
+  Mock -CommandName 'Save-ODUConfig' -MockWith { }
+
+  Mock -CommandName 'Get-ODUConfig' -MockWith { $Config }
+
+  It 'Confirm-ODUConfig returns $true' { Confirm-ODUConfig | Should Be $true  }
+
+  It 'Get-ODUConfig returns a hashtable' { Get-ODUConfig | Should BeOfType [hashtable] }
+
+  It 'Get-ODUConfig returns value for ExportRootFolder' { (Get-ODUConfig).ExportRootFolder | Should Be $ExportRootFolder }
+
+  It 'Get-ODUConfigOctopusServer throws error' { { Get-ODUConfigOctopusServer } | Should throw }
+
+  It 'Get-ODUConfigDecryptApiKey throws error' { { Get-ODUConfigDecryptApiKey } | Should throw }
+
+  It 'Initialize-ODUConfig returns $null and does nothing' { Initialize-ODUConfig | Should BeNullOrEmpty }
+
+  It 'Initialize-ODUConfig returns $null and does nothing' { Initialize-ODUConfig | Should BeNullOrEmpty }
+
+  It 'Test-ODUConfigFilePath returns $true' { Test-ODUConfigFilePath | Should Be $true }
+
 }
+#endregion
 
-Mock -CommandName 'Save-ODUConfig' -MockWith { }
 
-Mock -CommandName 'Get-ODUConfig' -MockWith { $Config }
+#region Configuration Octopus Server initialized
+Describe 'Configuration: only export root folder initialized' {
 
-It 'Confirm-ODUConfig returns $true' { Confirm-ODUConfig | Should Be $true  }
+  # ensure config file DOES exist
+  $ExportRootFolder = Join-Path -Path $TestDrive 'ExportRoot'
+  $ConfigFolderPath = Join-Path -Path $TestDrive 'Configuration'
+  $ConfigFilePath = Join-Path -Path $ConfigFolderPath 'Configuration.psd1'
 
-It 'Get-ODUConfig returns a hashtable' { Get-ODUConfig | Should BeOfType [hashtable] }
+  New-Item -Path $ExportRootFolder -ItemType Directory > $null
+  New-Item -Path $ConfigFolderPath -ItemType Directory > $null
+  
+  $OctoServerName = 'test.com'
+  $OctoServerUrl = 'https://test.com'
+  $OctoServerApiKey = 'API-1234567890'
+  $OctoServerApiKeyEncrypted = $OctoServerApiKey
+  if (($PSVersionTable.PSVersion.Major -le 5) -or ($true -eq $IsWindows)) {
+    $OctoServerApiKeyEncrypted = ConvertTo-SecureString -String $OctoServerApiKey -AsPlainText -Force | ConvertFrom-SecureString
+  }
 
-It 'Get-ODUConfig returns value for ExportRootFolder' { (Get-ODUConfig).ExportRootFolder | Should Be $ExportRootFolder }
 
-It 'Get-ODUConfigDecryptApiKey throws error' { { Get-ODUConfigDecryptApiKey } | Should throw }
+  $ConfigString = @"
+@{
+ExportRootFolder = $ExportRootFolder
+OctopusServers = @(
+  @{
+    Name = $OctoServerName
+    Url = $OctoServerUrl
+    ApiKey = $OctoServerApiKeyEncrypted
+    TypeBlackList = @('CommunityActionTemplates','Deployments','Events','Interruptions','Releases','Reporting','Tasks','Packages')
+    TypeWhiteList = @()
+    PropertyBlackList = @{ }
+    PropertyWhiteList = @{ }
+    LastPurgeCompareFolder = 'UNDEFINED'
+    Search = @{
+      CodeSearchPattern = 'UNDEFINED'
+      CodeRootPaths = 'UNDEFINED'
+    }
+  }
+)
+ExternalTools = @{
+  DiffViewerPath = 'UNDEFINED'
+  TextEditorPath = 'UNDEFINED'
+}
+ParallelJobsCount = 1
+}
+"@
+  Set-Content -Path $ConfigFilePath -Value $ConfigString
+  Mock -CommandName 'Get-ODUConfigFilePath' -MockWith { $ConfigFilePath }
 
-It 'Get-ODUConfigOctopusServer throws error' { { Get-ODUConfigOctopusServer } | Should throw }
+  $Config = @{
+    ExportRootFolder  = $ExportRootFolder
+    OctopusServers    = @(
+      @{
+        Name                   = $OctoServerName
+        Url                    = $OctoServerUrl
+        ApiKey                 = $OctoServerApiKeyEncrypted
+        TypeBlackList          = @('CommunityActionTemplates', 'Deployments', 'Events', 'Interruptions', 'Releases', 'Reporting', 'Tasks', 'Packages')
+        TypeWhiteList          = @()
+        PropertyBlackList      = @{ }
+        PropertyWhiteList      = @{ }
+        LastPurgeCompareFolder = 'UNDEFINED'
+        Search                 = @{
+          CodeSearchPattern = 'UNDEFINED'
+          CodeRootPaths     = 'UNDEFINED'
+        }
+      }
+    )
+    ExternalTools     = @{
+      DiffViewerPath = 'UNDEFINED'
+      TextEditorPath = 'UNDEFINED'
+    }
+    ParallelJobsCount = 1
+  }
 
-It 'Initialize-ODUConfig returns $null and does nothing' { Initialize-ODUConfig | Should BeNullOrEmpty }
+  Mock -CommandName 'Save-ODUConfig' -MockWith { }
 
-It 'Initialize-ODUConfig returns $null and does nothing' { Initialize-ODUConfig | Should BeNullOrEmpty }
+  Mock -CommandName 'Get-ODUConfig' -MockWith { $Config }
 
-It 'Test-ODUConfigFilePath returns $true' { Test-ODUConfigFilePath | Should Be $true }
+  It 'Confirm-ODUConfig returns $true' { Confirm-ODUConfig | Should Be $true  }
 
+  It 'Get-ODUConfig returns a hashtable' { Get-ODUConfig | Should BeOfType [hashtable] }
+
+  It 'Get-ODUConfig returns value for ExportRootFolder' { (Get-ODUConfig).ExportRootFolder | Should Be $ExportRootFolder }
+
+  It 'Get-ODUConfigOctopusServer returns a hashtable' { Get-ODUConfigOctopusServer | Should BeOfType [hashtable] }
+
+  It 'Get-ODUConfigOctopusServer.Url returns correct value' { (Get-ODUConfigOctopusServer).Url | Should Be $OctoServerUrl }
+
+  It 'Get-ODUConfigDecryptApiKey throws error' { Get-ODUConfigDecryptApiKey | Should Be $OctoServerApiKey }
+
+  It 'Initialize-ODUConfig returns $null and does nothing' { Initialize-ODUConfig | Should BeNullOrEmpty }
+
+  It 'Initialize-ODUConfig returns $null and does nothing' { Initialize-ODUConfig | Should BeNullOrEmpty }
+
+  It 'Test-ODUConfigFilePath returns $true' { Test-ODUConfigFilePath | Should Be $true }
 }
 #endregion
 
 
 
 
-
 # do in steps: 
-# nothing set
-# root export set, nothing else
-# then octo server
 # then individual tools
 #  nested contexts?
-
-# asdf need testsmock for partially configured system
-# no external tools, etc.
-
 
 
 
