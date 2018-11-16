0f6cd503600650797c28d39bbebf733a842b2b60
Dan Ward <dtward@gmail.com>
2018-11-16 18:01:28 -0500

    added Invoke-ODURestMethod

---
+++ OctopusDeployUtilities/Source/Private/Export_Rest.ps1
@@ -0,0 +1,35 @@
+
+
+#region Function: Invoke-ODURestMethod
+
+<#
+.SYNOPSIS
+Calls url with API key in header and returns results
+.DESCRIPTION
+Calls url with API key in header and returns results
+.PARAMETER Url
+Full url of REST API to call
+.PARAMETER ApiKey
+Unencrypted ApiKey to pass in REST API call headers
+.EXAMPLE
+Invoke-ODURestMethod -Url https://MyOctoServer.octopus.app -ApiKey 'API-123456789012345678901234567'
+<calls Url passing in ApiKey and returns results>
+#>
+function Invoke-ODURestMethod {
+  #region Function parameters
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Url,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ApiKey
+  )
+  #endregion
+  process {
+    Write-Verbose "Calling Url $Url with API Key (first 7 characters) $($ApiKey.Substring(0,7))"
+    Invoke-RestMethod -Method Get -Uri $Url -Headers @{ 'X-Octopus-ApiKey' = $ApiKey }
+  }
+}
+#endregion
