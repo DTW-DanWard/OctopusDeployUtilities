72ddbc3aaa196ffcd1535aa9298a6498469e8eef
Dan Ward <dtward@gmail.com>
2018-11-17 12:48:02 -0500

    initial add of Export_OctoApi_Public / Tests

---
+++ Tests/Export_OctoApi_Public.Tests.ps1
@@ -0,0 +1,22 @@
+Set-StrictMode -Version Latest
+
+#region Set module/script-level variables
+$ScriptLevelVariables = Join-Path -Path $env:BHModulePath -ChildPath 'Set-ScriptLevelVariables.ps1'
+. $ScriptLevelVariables
+#endregion
+
+
+#region Dot-source Source file associated with this test file
+# if no value returned just exit; specific error is already written in Get-SourceScriptFilePath call
+. $PSScriptRoot\Get-SourceScriptFilePath.ps1
+$SourceScript = Get-SourceScriptFilePath
+if ($null -eq $SourceScript) { exit }
+Describe "Re/loading: $SourceScript" { }
+. $SourceScript
+#endregion
+
+
+
+# asdf tests to do
+# validation on valid names
+
