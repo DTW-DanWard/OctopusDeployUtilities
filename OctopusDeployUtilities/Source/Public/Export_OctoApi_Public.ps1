ea3e4f298c15d75a4d4ee4bfe62d5bc0dc2d5139
Dan Ward <dtward@gmail.com>
2018-11-19 11:27:19 -0500

    added Test-ODUValidateRestApiTypeName

--- OctopusDeployUtilities/Source/Public/Export_OctoApi_Public.ps1
+++ OctopusDeployUtilities/Source/Public/Export_OctoApi_Public.ps1
@@ -20,8 +20,48 @@ FeaturesConfiguration
 #>
 function Get-ODURestApiTypeNames {
   [CmdletBinding()]
+  [OutputType([System.Array])]
   param()
   process {
     (Get-ODUStandardExportRestApiCalls).RestName | Sort-Object
   }
 }
+#endregion
+
+
+#region Function: Test-ODUValidateRestApiTypeName
+
+<#
+.SYNOPSIS
+Validates list of values against Type names used with Octopus Deploy REST API
+.DESCRIPTION
+Validates list of values against Type names used with Octopus Deploy REST API
+If all passed values are valid, returns $true, if any one is invalid, returns $false
+.EXAMPLE
+Test-ODUValidateRestApiTypeName Projects
+$true
+Test-ODUValidateRestApiTypeName Projects, Variables
+$true
+Test-ODUValidateRestApiTypeName Projects, Variables, asdfasdfasdf
+$false
+#>
+function Test-ODUValidateRestApiTypeName {
+  [CmdletBinding()]
+  [OutputType([bool])]
+  param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]$TypeName
+  )
+  process {
+    $ValidTypeNames = Get-ODURestApiTypeNames
+    $Valid = $true
+    $TypeName | ForEach-Object {
+      if ($_ -notin $ValidTypeNames) {
+        $Valid = $false
+      }
+    }
+    $Valid
+  }
+}
+#endregion
