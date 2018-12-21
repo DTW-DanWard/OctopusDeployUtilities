708096b470070ef586c7c9fcd4b49721ccd4bd9d
Dan Ward <dtward@gmail.com>
2018-12-21 18:11:13 -0500

    exclude PoshRSJob from Import-Module step (breaks tab autocomplete)

--- OctopusDeployUtilities/Install-ExternalRuntimeDependencies.ps1
+++ OctopusDeployUtilities/Install-ExternalRuntimeDependencies.ps1
@@ -7,5 +7,9 @@
 'Configuration', 'PoshRSJob' | ForEach-Object {
   $ProgressPreference = 'SilentlyContinue'
   if ($null -eq (Get-Module -Name $_ -ListAvailable)) { Install-Module -Name $_ -Force -AllowClobber }
-  Import-Module -Name $_ -Force
+  # Configuration needs to be manually imported but
+  # do NOT import module PoshRSJob; it will work fine without being imported but
+  # more importantly, if you -Force import OctopusDeployUtilities module again (code change in dev),
+  # tab autocompletion stops in the console.  go figure.
+  if (($_ -ne 'PoshRSJob') -and ($null -eq (Get-Module -Name $_))) { Import-Module -Name $_ }
 }
