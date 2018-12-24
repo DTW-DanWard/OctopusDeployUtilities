2e7f7007798c7ffa87ce80171c18622987a8b44e
Dan Ward <dtward@gmail.com>
2018-12-24 09:52:33 -0500

    fixed comment

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -12,7 +12,7 @@
 
 # Set-BuildEnvironment from BuildHelpers module has populated ENV:BHProjectName
 
-# only run on build server on branch master; don't run if !deploy in commit message
+# only run on build server on branch master with !deploy in commit message
 if ($env:BHBuildSystem -ne 'Unknown' -and $env:BHBranchName -eq 'master' -and $env:BHCommitMessage -match '!deploy') {
   Deploy Module {
     By PSGalleryModule {
