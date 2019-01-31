b6bbb19167fcc1524f5a39b908b90ad520abb476
Dan Ward <dtward@gmail.com>
2019-01-31 09:37:04 -0500

    update code coverage badge regex: don't overwrite following badges

--- InvokeBuild.ps1
+++ InvokeBuild.ps1
@@ -26,15 +26,15 @@ function Update-CodeCoveragePercent {
   )
 
   $BadgeColor = switch ($CodeCoverage) {
-    {$_ -in 90..100} { 'brightgreen' }
-    {$_ -in 75..89} { 'yellow' }
-    {$_ -in 60..74} { 'orange' }
+    {$_ -in 80..100} { 'brightgreen' }
+    {$_ -in 65..79} { 'yellow' }
+    {$_ -in 50..64} { 'orange' }
     default { 'red' }
   }
 
   if ($PSCmdlet.ShouldProcess($TextFilePath)) {
     $ReadmeContent = Get-Content $TextFilePath
-    $ReadmeContent = $ReadmeContent -replace "!\[Test Coverage\].+\)", "![Test Coverage](https://img.shields.io/badge/coverage-$CodeCoverage%25-$BadgeColor.svg?maxAge=60)"
+    $ReadmeContent = $ReadmeContent -replace "!\[Test Coverage\].+\) *", "![Test Coverage](https://img.shields.io/badge/coverage-$CodeCoverage%25-$BadgeColor.svg?maxAge=60)"
     $ReadmeContent | Set-Content -Path $TextFilePath
   }
 }
