40a319772627b4d8cb8a3e00ddbcb3394f1e3505
Dan Ward <dtward@gmail.com>
2019-01-31 17:47:53 -0500

    add machines to environments tests

--- OctopusDeployUtilities/Source/Private/JoinData_MachineEnvironments.ps1
+++ OctopusDeployUtilities/Source/Private/JoinData_MachineEnvironments.ps1
@@ -19,9 +19,8 @@ function Update-ODUExportAddMachinesToEnvironment {
   [CmdletBinding()]
   [OutputType([string])]
   param(
-    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string]$Path
+    [string]$Path = $(throw "$($MyInvocation.MyCommand) : missing parameter Path")
   )
   #endregion
   process {
